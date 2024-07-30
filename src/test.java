f4997c3788237cea999a1a30e32cb37e2d103d7a
thomas <1973426731@qq.com>
2024-07-30 15:57:28 +0800

    leetcode，7月30日第二次修改

--- src/test.java
+++ src/test.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class test {
     public static void main(String[] args) {
-        int[][] edges = new int[][] {{0,3,10},{2,3,2},{3,3,6},{3,3,9},{1,3,2},{3,0,4}};
-        System.out.println(edges.length);
+        int[][] variables = {{55,19,87,22}};
+        int target = 34, n = 1, p = target;
+        while(p >= 10){
+            p = p / 10;
+            n += 1;
+        }
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0;i<variables.length; i++){
+            variables[i][0] = variables[i][0] % (10*n);
+            if(Math.pow(Math.pow(variables[i][0], variables[i][1]) % 10, variables[i][2]) % variables[i][3] == target){
+                result.add(i);
+            }
+        }
+        System.out.println(result);
     }
 }
