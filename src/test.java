fa7f219c9a298f7cf26fb16c55492bf438afde0c
thomas <1973426731@qq.com>
2024-07-30 15:09:05 +0800

    leetcode，7月30日第一次修改

---
+++ src/test.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class test {
+    public static void main(String[] args) {
+        int[][] edges = new int[][] {{0,3,10},{2,3,2},{3,3,6},{3,3,9},{1,3,2},{3,0,4}};
+        System.out.println(edges.length);
+    }
+}
